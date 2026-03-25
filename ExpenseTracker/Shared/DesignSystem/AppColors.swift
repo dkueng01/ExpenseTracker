205e48fb979d1a7778807f6f9196430872a2a872
dkueng <davidwichtig@gmx.at>
2026-03-25 11:13:39 +0100

    Updated theme colors

--- ExpenseTracker/Shared/DesignSystem/AppColors.swift
+++ ExpenseTracker/Shared/DesignSystem/AppColors.swift
@@ -3,12 +3,12 @@ import SwiftUI
 enum AppColors {
     static let screenBackground = Color(.systemGroupedBackground)
 
-    static let cardBackground = Color(.systemBackground)
-    static let elevatedBackground = Color(.systemBackground)
-    static let inputBackground = Color(.systemBackground)
+    static let cardBackground = Color(.secondarySystemGroupedBackground)
+    static let elevatedBackground = Color(.secondarySystemGroupedBackground)
+    static let inputBackground = Color(.secondarySystemBackground)
 
-    static let cardBorder = Color(.separator).opacity(0.10)
-    static let controlBorder = Color(.separator).opacity(0.18)
+    static let cardBorder = Color(.separator).opacity(0.18)
+    static let controlBorder = Color(.separator).opacity(0.24)
 
     static let primaryTint = Color.blue
     static let disabledButton = Color.gray.opacity(0.4)
